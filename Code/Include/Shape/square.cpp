449cd112f62434b03d80d2dda6e44200fab54a9e
EnzoGuillouche <145991192+EnzoGuillouche@users.noreply.github.com>
2025-04-22 17:43:44 +0200

    Fixed issues

--- Code/Include/Shape/square.cpp
+++ Code/Include/Shape/square.cpp
@@ -1,18 +1,18 @@
 #include "square.hpp"
 
 Square::Square() {
-        this->shapesTriangles = 2;
+    this->shapesTriangles = 2;
 
-        // Define the vertices of a square, each consisting of x, y, and z coordinates
-        this->vertices = {
-            0.5f, 0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            -0.5f, 0.5f, 0.0f
-        };
-
-        this->indices = {
-            0, 1, 3,
-            1, 2, 3
-        };
+    // Define the vertices of a square, each consisting of x, y, and z coordinates
+    this->vertices = {
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f
     };
+
+    this->indices = {
+        0, 1, 3,
+        1, 2, 3
+    };
+};
--- Code/Include/Shape/triangle.cpp
+++ Code/Include/Shape/triangle.cpp
@@ -1,16 +1,16 @@
 #include "triangle.hpp"
 
 Triangle::Triangle() {
-        this->shapesTriangles = 1;
+    this->shapesTriangles = 1;
 
-        // Define the vertices of a Triangle, each consisting of x, y, and z coordinates
-        std::vector<float> vertices = {
-            0.0f, 0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-        };
-
-        std::vector<unsigned int> indices = {
-            0, 1, 2,
-        };
+    // Define the vertices of a Triangle, each consisting of x, y, and z coordinates
+    this->vertices = {
+        0.0f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
     };
+
+    this->indices = {
+        0, 1, 2,
+    };
+};
